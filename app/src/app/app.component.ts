437a6e4b8617cf91947800c04a71efd356df9712
nirosha <niroshakannan17@gmail.com>
2024-04-04 21:46:25 +0530

    create and read operation angular

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app';
+
+  to_do_action = '';
+
+  to_do_array:string[]= [];
+
+  saveTodoList()
+  {
+    this.to_do_array.push(this.to_do_action);
+    console.log(this.to_do_array);
+    this.to_do_action = '';
+  }
+
 }
